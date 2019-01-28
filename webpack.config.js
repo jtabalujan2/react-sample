3a86ed730a339ff868a61d30aaeb56f96b317b28
Julian T <heyyytabalu@yahoo.com>
2019-01-28 15:25:21 -0800

    Adding HTML, correct version dependencies and webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
-const webpack = require('webpack')
 const path = require('path')
+const webpack = require('webpack')
 
-const config = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
+module.exports = {
+    entry: './src/index.js',
+    mode: "development",
+    modules: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                excludes: /node_modules/,
-                use: 'babel-loader'
+                exclude: /(node_modules|bower_components)/,
+                loader: babel-loader,
+                options: {presets: ["@babel/env"]}
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader","css-loader"]
             }
         ]
-    }
+    },
+    resolve: {extensions: ["*",".js",".jsx"]},
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist",
+        hotOnly: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 }
-
-module.exports = config
