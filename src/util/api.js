c550280e0122c3b42c21b47379dc46098b5fc439
Julian T <heyyytabalu@yahoo.com>
2019-02-24 18:00:19 -0800

    Adding API functions

--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,19 @@
 import axios from 'axios'
 
+//getUserInfo
+//getRepoInfo
+//getStarCount 
+//star count
+
+
+
+
 const Methods = {
+
+    battle : function() {
+
+    },
+
     fetchPopularRepos: function (language) {
         let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories')
     
